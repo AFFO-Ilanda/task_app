549961b901f7498c721f668a575cd204232865ef
AFFO-Ilanda <workstaff020@gmail.com>
2026-01-08 10:35:36 +0000

    init.php

--- includes/init.php
+++ includes/init.php
@@ -8,11 +8,7 @@ function e(string $s): string {
   return htmlspecialchars($s, ENT_QUOTES, 'UTF-8');
 }
 
-/**
- * URL depuis la racine du projet /task_app
- * Ex: url('public/dashboard.php') => /task_app/public/dashboard.php
- * Empêche le bug public/public
- */
+
 function url(string $path = ''): string {
   $dir = rtrim(str_replace('\\', '/', dirname($_SERVER['SCRIPT_NAME'])), '/');
 
